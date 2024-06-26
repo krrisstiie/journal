dc1cb47ec5821850850d9aa9df48eb7e07c11855
krrisstiie <kristiefongg@gmail.com>
2024-06-26 15:21:00 -0700

    finished everything except for colors and background (I THINK)

--- journal/freeResponse.swift
+++ journal/freeResponse.swift
@@ -8,12 +8,19 @@
 import SwiftUI
 
 struct freeResponse: View {
+    
+    @State private var freeResponse = ""
+    
     var body: some View {
-        Text("Type to your heart's content")
-            .padding(.top)
-        Spacer()
+        ScrollView() {
+            Text("Type to your heart's content. Let everything out, because once you leave the app, everything you wrote will be deleted. 🔥")
+                .multilineTextAlignment(.center)
+                .padding(.all)
+            TextField("Type your response here", text: $freeResponse)
+                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            Spacer()
             
-            
+        }
     }
 }
 
--- journal/prompt.swift
+++ journal/prompt.swift
@@ -8,11 +8,35 @@
 import SwiftUI
 
 struct prompt: View {
+
+    @State private var response = ""
+    @State private var response2 = ""
+    @State private var response3 = ""
+
     var body: some View {
-        Text("Describe your ideal stress-free day. What does it look like, and who else is there?")
-            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-        Spacer()
+        VStack {
+        ScrollView() {
             
+            Text("Describe my ideal stress-free day. What does it look like, and who else is there?")
+                .multilineTextAlignment(.center)
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                TextField("Type your response here", text: $response)
+                .padding(/*@START_MENU_TOKEN@*/.all, 110.0/*@END_MENU_TOKEN@*/)
+                
+            Text("Are there any triggers that amplify my stress? How can I address or avoid them?")
+                .multilineTextAlignment(.center)
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                TextField("Type your response here", text: $response2)
+                .padding(/*@START_MENU_TOKEN@*/.all, 110.0/*@END_MENU_TOKEN@*/)
+            
+            Text("Are there any triggers that amplify my stress? How can I address or avoid them?")
+                .multilineTextAlignment(.center)
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                TextField("Type your response here", text: $response2)
+                .padding(/*@START_MENU_TOKEN@*/.all, 110.0/*@END_MENU_TOKEN@*/)
+        }
+        .padding(.bottom)
+        }
     }
 }
 
