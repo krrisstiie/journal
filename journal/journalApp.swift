f207426d1c5d4e6a40f08bbbb29674903b7801a6
krrisstiie <kristiefongg@gmail.com>
2024-06-26 12:38:37 -0700

    Initial Commit

---
+++ journal/journalApp.swift
@@ -0,0 +1,17 @@
+//
+//  journalApp.swift
+//  journal
+//
+//  Created by Scholar on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct journalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
